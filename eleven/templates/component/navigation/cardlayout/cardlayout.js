c36f48c86fdd91fd74bcdfe7703f10b2baa2c07e
mortendk <morten@geekroyale.com>
2017-06-14 10:37:28 +0200

    #94 card layout chooser

---
+++ eleven/templates/component/navigation/cardlayout/cardlayout.js
@@ -0,0 +1,40 @@
+// show hide the sidebar
+(function ($) {
+
+  var widthClasses = "LW-1 LW-2 LW-3 LW-4 LW-5 LW-6 LW-7 LW-8 LW-9 LW-10 LW-11 LW-12"
+
+  // grap a cookie set button to active
+  if( $.cookie("eleven-cardLayout") ){
+    $("a[data-layout="  + $.cookie('eleven-cardLayout') + "]" ).addClass("is-active");
+    $( ".js-card-layout" ).removeClass( widthClasses ).addClass( 'LW-' + $.cookie("eleven-cardLayout") );
+  }
+
+  // change layout
+  $(".js-cardlayout" ).click(function(e) {
+
+    var layout = $(this).attr("data-layout");
+    $( ".js-card-layout-target" ).removeClass( widthClasses ).addClass( 'LW-' + layout );
+
+    //color buttons
+    $(".js-cardlayout").removeClass("is-active");
+    $(this).toggleClass("is-active");
+
+    //umm cookies
+    $.cookie("eleven-cardLayout", layout);
+
+    e.preventDefault();
+  });
+
+  // hide show card text
+  $( ".js-hide-cardtext" ).click(function(e) {
+    $('.card__text').toggleClass('card__text--hide');
+    $(this).toggleClass("is-active");
+
+
+    e.preventDefault();
+  });
+
+
+
+
+}(jQuery))
