e9f3c660fe69e5c6c840809add7cf19114296bb8
mortendk <morten@geekroyale.com>
2017-08-03 12:40:29 +0200

    poor mans version of fixing the default fonts in ckeditor

--- eleven/templates/layout-landing/node/node-edit-form.js
+++ eleven/templates/layout-landing/node/node-edit-form.js
@@ -1,6 +1,10 @@
 // show hide the sidebar
 (function ($) {
 
+  // $('iframe').contents().find('body').css("margin", "40px");
+
+  $("iframe").contents().find("p").css('color','red');
+
   var widthClasses = "w-1 w-2 w-3 w-4 w-5 w-6 w-7 w-8 w-9 w-10 w-11 w-12"
 
   // grap a cookie set button to active
